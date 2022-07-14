87444bfed5679acf3ea593a2233591801954203e
Anton <deftume@google.com>
2022-07-14 14:12:28 +0300

    feat: the number of passing tests 100%

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let result = [];
+    matrix.forEach((element, index) => {
+      let temp = [];
+      for (let number of element) {
+        temp.push(number);
+      }
+      index % 2 === 0 ? result.push(...temp):result.push(...temp.reverse())
+    });
+  return result;
 }
